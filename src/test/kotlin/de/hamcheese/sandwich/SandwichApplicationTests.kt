1e486277e07f7fb8d8ba0d5f78dbaa8a49de8620
Burger <Sebastian.Burger@otto.de>
2019-10-28 09:41:04 +0100

    Changed to Kotlin and added H2

---
+++ src/test/kotlin/de/hamcheese/sandwich/SandwichApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.hamcheese.sandwich
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SandwichApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
