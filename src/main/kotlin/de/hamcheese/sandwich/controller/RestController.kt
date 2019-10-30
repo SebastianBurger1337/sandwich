3e1d17065dc8b7a1641dd16f627843a11586b7e3
Burger <Sebastian.Burger@otto.de>
2019-10-30 21:25:52 +0100

    Added funny cat picture redirect

--- src/main/kotlin/de/hamcheese/sandwich/controller/RestController.kt
+++ src/main/kotlin/de/hamcheese/sandwich/controller/RestController.kt
@@ -5,13 +5,14 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
 
 @RestController
 public class RestController {
 
     @GetMapping("/health")
-    public fun getHealth():ResponseEntity<String> {
-        return ResponseEntity.ok("OK")
+    public fun getHealth():RedirectView {
+        return RedirectView("https://http.cat/200.jpg")
     }
 
 }
