9d90abbb6a01dcbf2dba0bf47dae41f5686f32fa
Burger <Sebastian.Burger@otto.de>
2019-11-15 10:26:01 +0100

    Added Order entity

--- src/main/kotlin/de/hamcheese/sandwich/entity/Order.kt
+++ src/main/kotlin/de/hamcheese/sandwich/entity/Order.kt
@@ -1,4 +1,18 @@
 package de.hamcheese.sandwich.entity
 
-class Order {
-}
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+public class Order(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        var orderId: Int,
+        var products: List<Sandwich>,
+        var customerId: Int,
+        var delivery: Boolean,
+        var totalPrice: Int,
+        var tax: Int
+)
